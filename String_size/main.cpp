e15609dbbd11fa0eba0baff2275b090729e43c73
SzalanskiIzold <91244116+SzalanskiIzold@users.noreply.github.com>
2021-10-19 09:41:44 +0300

    Add files via upload

---
+++ String_size/main.cpp
@@ -0,0 +1,15 @@
+
+ #include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name("Kis Pista") ;
+    cout << "What is your name";
+    cin>> name ;
+    cout <<"Hello,"<< name<< "!";
+    cout << name.size() ;
+        return 0;
+}
+
