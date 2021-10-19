2507cdc58a46af63cc449e1c561fdf6e4e20925e
SzalanskiIzold <91244116+SzalanskiIzold@users.noreply.github.com>
2021-10-19 09:39:40 +0300

    Add files via upload

---
+++ String_append/main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name ;
+     name="Kis" ;
+     string keresztnev = "Pista Gyozo";
+     name.append(" ");
+     name.append("Bela");
+     cout << name <<endl ;
+     name.append(keresztnev) ;
+     cout << name<< endl;
+
+
+        return 0;
+}
