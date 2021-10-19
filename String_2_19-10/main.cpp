169f04df5a73f62e7f782ac6a8f66ea39e957d0c
SzalanskiIzold <91244116+SzalanskiIzold@users.noreply.github.com>
2021-10-19 09:39:23 +0300

    Add files via upload

---
+++ String_2_19-10/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name ;
+    cout << "What is your name";
+     name= "KIs pista" ;
+    cout <<"Hello,"<< name<< "!/n" ;    return 0;
+}
