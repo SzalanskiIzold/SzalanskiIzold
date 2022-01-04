1f41196083175cccddd9cba34edeb63c4fa87da6
SzalanskiIzold <91244116+SzalanskiIzold@users.noreply.github.com>
2022-01-04 09:41:47 +0200

    Add files via upload
    5. Írjunk egy rekurzív függvényt, amely kiszámolja egy tízes számrendszerbeli szám számjegyeinek szorzatát

---
+++ rekurzio5fel/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int szam(int n){
+if(n==0){
+    return 1;
+}
+return n%10*szam(n/10 ) ;
+   }
+
+int main()
+{
+    cout<< szam(124) ;
+    return 0;
+}
+
