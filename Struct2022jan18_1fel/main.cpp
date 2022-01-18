ee96900d014b003a84f94af0d1fa92def5e26a1f
SzalanskiIzold <91244116+SzalanskiIzold@users.noreply.github.com>
2022-01-18 09:14:47 +0200

    Add files via upload

---
+++ Struct2022jan18_1fel/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+struct diak{
+int azonosito;
+string nev;
+string osztaly;
+double media;
+} a,b,c,d;
+int main()
+{
+    cout << "a.nev=" ;
+    cin>> a.nev;
+    cout << "a.media=" ;
+    cin>> a.media;
+    cout << "b.nev=";
+    cin>> b.nev;
+    cout << "b.media=" ;
+    cin>> b.media;
+    cout << "c.nev=" ;
+    cin>> c.nev;
+    cout << "c.media=" ;
+    cin>> c.media;
+    if(a.media>b.media && a.media>c.media){
+        cout<< a.nev ;
+
+    }else if(b.media>c.media){
+         cout<< b.nev ;
+    }else{
+      cout<< c.nev;
+    }
+    return 0;
+}
