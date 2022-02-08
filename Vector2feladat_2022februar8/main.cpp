77eeb361ab68f61b8d6cb8096767b73e638e5f25
SzalanskiIzold <91244116+SzalanskiIzold@users.noreply.github.com>
2022-02-08 09:24:59 +0200

    Hozzunk létre egy vektort, töltsük fel n elemmel, majd olvassunk be egy k számot s töröljük ki az utolsó k elemét, aztán írassuk ki!
     Hozzunk létre egy vektort, töltsük fel n elemmel, majd olvassunk be egy k számot s töröljük ki az utolsó k elemét, aztán írassuk ki!

---
+++ Vector2feladat_2022februar8/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n, k, x;
+    vector<int> tomb ;
+    cout<<" n=" ;
+    cin>> n;
+    for(int i=0; i<n; i++){
+        cout<<"tomb["<<i<<"]=" ;
+        cout<<"x=" ;
+        cin>> x ;
+
+        tomb.push_back(x) ;
+    }
+    cout<<"k=" ;
+    cin>> k;
+
+    for(int i=0; i<k; i++){
+             tomb.pop_back() ;
+    }
+    n=n-k ;
+    for(int i=0; i<n; i++){
+            cout<< tomb[i]<<" " ;
+    }
+
+    return 0;
+}
+
