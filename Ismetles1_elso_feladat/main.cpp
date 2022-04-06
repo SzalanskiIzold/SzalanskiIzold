9bbdcaf2773013256f08786c96d04910ae92d36f
SzalanskiIzold <91244116+SzalanskiIzold@users.noreply.github.com>
2022-04-06 20:41:41 +0300

    Add files via upload

---
+++ Ismetles1_elso_feladat/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include<fstream>
+
+using namespace std;
+
+int main()
+{
+   ifstream in ("in.txt");
+   ofstream out ("output.txt");
+
+   int a1, a2, v[100], n, min=99, max=0;
+   in>>n;
+   for (int i=0; i<100; i++){
+    v[i]=0;
+   }
+   for(int i=0; i<n; i++){
+    in>>a1;
+    in>>a2;
+
+
+    if(a1<min){
+        min=a1;
+    }
+    if(a2>max){
+        max=a2;
+    }
+    for(int j=a1; j<a2+1; j++){
+        v[j]=1;
+
+    }
+   }
+
+
+   int S=0;
+   for(int i=min; i<max; i++){
+    if(v[i]== 1 && v[i+1]==0){
+
+        S++;
+
+
+
+   }}
+   out<<S;
+
+   in.close();
+    out.close();
+    return 0;
+}
