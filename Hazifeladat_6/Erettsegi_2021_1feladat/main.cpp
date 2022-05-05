5b9c254ca3a8584e80559559108c4456b9d23225
SzalanskiIzold <91244116+SzalanskiIzold@users.noreply.github.com>
2022-05-05 07:26:43 +0300

    Add files via upload

---
+++ Hazifeladat_6/Erettsegi_2021_1feladat/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+int divprim(int n){
+int oszto =2 ;
+int S=0;
+while(n>1){
+    int szamlalo=0 ;
+    while(n%oszto==0){
+        n/=oszto ;
+        szamlalo++ ;
+    }
+    if(szamlalo%2>0){
+        S=S+oszto ;
+
+    }
+    oszto++ ;
+}
+return S ;
+
+}
+int main()
+{
+    int s=divprim(360) ;
+    cout << s<< endl ;
+    return 0;
+}
